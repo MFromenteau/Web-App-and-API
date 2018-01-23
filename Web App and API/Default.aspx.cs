c706e6ef31486caba1ebf42b3853da142c45c63b
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2018-01-23 11:09:39 +0100

    WebApp call API

--- Web App and API/Default.aspx.cs
+++ Web App and API/Default.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -21,7 +23,22 @@ namespace Web_App_and_API
             try
             {
                 address = this.txt_address.Text;
-                this.txt_result.Text = $"Results for {address}";
+                var httpWR = (HttpWebRequest)WebRequest.Create("http://localhost:51344/api/Address");
+                httpWR.ContentType = "application/json";
+                httpWR.Method = "POST";
+                using (var streamWriter = new StreamWriter(httpWR.GetRequestStream()))
+                {
+                    string json = $"{{Content:\"{address}\"}}";
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+                var httpResp = (HttpWebResponse)httpWR.GetResponse();
+                using(var streamReader = new StreamReader(httpResp.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    this.txt_result.Text = $"Results for {address} : {result}";
+                }
             }
             catch(Exception exception)
             {
---
+++ WebAPI/Controllers/AddressController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class Address
+    {
+        public string Content { get; set; }
+    }
+
+    public class AddressController : ApiController
+    {
+
+        public string Post([FromBody]Address address)
+        {
+            string results;
+            string a;
+
+            try
+            {
+                results = $"Results from API : {address.Content}";
+                return results;
+            }
+            catch (Exception exception)
+            {
+                return exception.ToString();
+            }
+        }
+    }
+}
